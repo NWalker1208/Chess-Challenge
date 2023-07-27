a2958a3dd2a527805abb3f0f3e2bc099f15c17ec
Nathan Walker <nathanwalker1208@gmail.com>
2023-07-26 19:56:17 -0600

    Moved away from extension methods to support heuristic property

--- Chess-Challenge/src/My Bot/MinimaxBot.cs
+++ Chess-Challenge/src/My Bot/MinimaxBot.cs
@@ -3,45 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+internal delegate double Heuristic(Board board);
+
 internal class MinimaxBot : IChessBot
 {
+    /// <summary>
+    /// Board heuristic used during minimax search.
+    /// </summary>
+    public Heuristic Heuristic { get; set; } = DefaultHeuristic;
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
         return BestMove(board, maxDepth: 5, maxBreadth: 12);
     }
 
-    private Move BestMove(Board board, int maxDepth = -1, int maxBreadth = 0)
+    /// <summary>
+    /// Performs a minimax search up to a maximum depth and returns the best move for the current board state.
+    /// </summary>
+    /// <param name="board">Current board state.</param>
+    /// <param name="maxDepth">Maximum search depth. Values less than 0 are treated as infinity.</param>
+    /// <param name="maxBreadth">Maximum search breadth (number of moves considered per turn). Values less than 1 are treated as infinity.</param>\
+    /// <returns>Best move found for the current board state.</returns>
+    public Move BestMove(Board board, int maxDepth = -1, int maxBreadth = 0)
     {
-        double alpha = double.NegativeInfinity, beta = double.PositiveInfinity;
-
-        Comparison<double> scoreComparison = board.GetScoreComparison();
-        double bestScore = board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
-
-        IEnumerable<Move> moves = board.GetSortedMoves();
-        if (maxBreadth >= 1) moves = moves.Take(maxBreadth);
-        Move bestMove = moves.First();
-
-        foreach (Move move in moves)
-        {
-            board.MakeMove(move);
-            double score = MiniMax(board, alpha, beta, maxDepth, maxBreadth);
-            board.UndoMove(move);
-
-            if (scoreComparison(score, bestScore) > 0)
-            {
-                (bestScore, bestMove) = (score, move);
-
-                if (board.IsWhiteToMove) alpha = Math.Max(alpha, bestScore);
-                else beta = Math.Min(beta, bestScore);
-            }
-        }
-
+        MiniMax(board, double.NegativeInfinity, double.PositiveInfinity, maxDepth, maxBreadth, out Move bestMove);
         return bestMove;
     }
 
     /// <summary>
-    /// Performs the minimax algorithm up to a maximum search depth.
+    /// Performs a minimax search up to a maximum depth.
     /// Based on https://en.wikipedia.org/wiki/Minimax and https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning.
     /// </summary>
     /// <param name="board">Current board state.</param>
@@ -49,23 +40,29 @@ internal class MinimaxBot : IChessBot
     /// <param name="alpha">Upper-bound for alpha-beta pruning.</param>
     /// <param name="maxDepth">Maximum search depth. Values less than 0 are treated as infinity.</param>
     /// <param name="maxBreadth">Maximum search breadth (number of moves considered per turn). Values less than 1 are treated as infinity.</param>
+    /// <param name="bestMove">Best move found for the current board state.</param>
     /// <returns>Minimax value of board state.</returns>
-    private double MiniMax(Board board, double alpha, double beta, int maxDepth, int maxBreadth)
+    public double MiniMax(Board board, double alpha, double beta, int maxDepth, int maxBreadth, out Move bestMove)
     {
-        if (maxDepth == 0 || board.IsGameOver()) return board.GetHeuristic();
+        if (maxDepth == 0 || board.IsGameOver())
+        {
+            bestMove = Move.NullMove;
+            return Heuristic(board);
+        }
         
         if (maxDepth > 0) maxDepth--;
 
-        Comparison<double> scoreComparison = board.GetScoreComparison();
+        Comparison<double> scoreComparison = GetScoreComparison(board);
         double bestScore = board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
 
-        IEnumerable<Move> moves = board.GetSortedMoves();
+        IEnumerable<Move> moves = GetSortedMoves(board);
         if (maxBreadth >= 1) moves = moves.Take(maxBreadth);
+        bestMove = moves.First();
 
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            double score = MiniMax(board, alpha, beta, maxDepth, maxBreadth);
+            double score = MiniMax(board, alpha, beta, maxDepth, maxBreadth, out _);
             board.UndoMove(move);
 
             if (scoreComparison(score, bestScore) > 0)
@@ -81,31 +78,51 @@ internal class MinimaxBot : IChessBot
 
         return bestScore;
     }
-}
 
-internal static class BoardExtensions
-{
+    /// <summary>
+    /// Calculates a heuristic for the given move based on the board state.
+    /// Equivalent to the heuristic of the board after the move has been made.
+    /// </summary>
+    /// <param name="board">Board state in which move will be made.</param>
+    /// <param name="move">Move to evaluate.</param>
+    /// <returns>Heuristic score for move.</returns>
+    private double MoveHeuristic(Board board, Move move)
+    {
+        board.MakeMove(move);
+        double score = Heuristic(board);
+        board.UndoMove(move);
+        return score;
+    }
+
+    /// <summary>
+    /// Gets the legal moves given the current board state and sorts them from best to worst (from the current player's perspective).
+    /// </summary>
+    /// <param name="board">Current board state.</param>
+    /// <returns>Sorted array of legal moves.</returns>
+    private Move[] GetSortedMoves(Board board)
+    {
+        Comparison<double> scoreComparison = GetScoreComparison(board);
+        Move[] moves = board.GetLegalMoves();
+        Array.Sort(moves.Select(m => MoveHeuristic(board, m)).Select(s => -s).ToArray(), moves, Comparer<double>.Create(scoreComparison));
+        return moves;
+    }
+
     /// <summary>
     /// Gets a <see cref="SimpleComparison"/> for comparing whether one score is better than another.
+    /// On white's turn, moves with higher (more positive) scores are better.
+    /// On black's turn, moves with lower (more negative) scores are better.
     /// </summary>
     /// <param name="board">Current board state.</param>
     /// <returns>Score comparison function.</returns>
-    public static Comparison<double> GetScoreComparison(this Board board) => board.IsWhiteToMove ? (a, b) => a.CompareTo(b) : (a, b) => b.CompareTo(a);
+    private static Comparison<double> GetScoreComparison(Board board) => board.IsWhiteToMove ? (a, b) => a.CompareTo(b) : (a, b) => b.CompareTo(a);
 
     /// <summary>
-    /// Determines whether the game has ended.
-    /// </summary>
-    /// <param name="board">Current board state.</param>
-    /// <returns>Whether the game has ended.</returns>
-    public static bool IsGameOver(this Board board) => board.IsInCheckmate() || board.IsDraw();
-
-    /// <summary>
-    /// Calculates a heuristic for the board state.
+    /// Calculates a simple heuristic for the board state based on piece values.
     /// Based on https://www.chess.com/terms/chess-piece-value.
     /// </summary>
     /// <param name="board">Board state to evaluate.</param>
     /// <returns>Heuristic score for board.</returns>
-    public static double GetHeuristic(this Board board)
+    public static double DefaultHeuristic(Board board)
     {
         if (board.IsInCheckmate()) return board.IsWhiteToMove ? -1000 : 1000;
         if (board.IsDraw()) return 0;
@@ -119,32 +136,14 @@ internal static class BoardExtensions
 
         return score;
     }
+}
 
+internal static class BoardExtensions
+{
     /// <summary>
-    /// Calculates a heuristic for the given move based on the board state.
-    /// Equivalent to the heuristic of the board after the move has been made.
-    /// </summary>
-    /// <param name="board">Board state in which move will be made.</param>
-    /// <param name="move">Move to evaluate.</param>
-    /// <returns>Heuristic score for move.</returns>
-    public static double GetMoveHeuristic(this Board board, Move move)
-    {
-        board.MakeMove(move);
-        double score = board.GetHeuristic();
-        board.UndoMove(move);
-        return score;
-    }
-
-    /// <summary>
-    /// Gets the legal moves given the current board state sorted from best to worst.
+    /// Determines whether the game has ended.
     /// </summary>
     /// <param name="board">Current board state.</param>
-    /// <returns>Sorted array of legal moves.</returns>
-    public static Move[] GetSortedMoves(this Board board)
-    {
-        Comparison<double> scoreComparison = board.GetScoreComparison();
-        Move[] moves = board.GetLegalMoves();
-        Array.Sort(moves.Select(board.GetMoveHeuristic).Select(s => -s).ToArray(), moves, Comparer<double>.Create(scoreComparison));
-        return moves;
-    }
+    /// <returns>Whether the game has ended.</returns>
+    public static bool IsGameOver(this Board board) => board.IsInCheckmate() || board.IsDraw();
 }
