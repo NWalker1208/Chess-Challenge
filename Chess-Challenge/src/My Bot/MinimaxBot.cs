fce71f534c4e72687eb7773ecbab83e23bf2d4a3
Nathan Walker <nathanwalker1208@gmail.com>
2023-07-26 18:48:08 -0600

    Moved minimax implementation to separate file

---
+++ Chess-Challenge/src/My Bot/MinimaxBot.cs
@@ -0,0 +1,88 @@
+﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+
+internal class MinimaxBot : IChessBot
+{
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] moves = board.GetLegalMoves();
+        return BestMove(board, maxDepth: 3);
+    }
+
+    private delegate bool SimpleComparison(double a, double b);
+
+    private static bool IsGreater(double a, double b) => a > b;
+    private static bool IsLess(double a, double b) => a < b;
+
+    private Move BestMove(Board board, int maxDepth = -1)
+    {
+        Move[] moves = board.GetLegalMoves();
+
+        SimpleComparison isBetter = board.IsWhiteToMove ? IsGreater : IsLess;
+        double bestScore = board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
+        Move bestMove = moves[0];
+
+        foreach (Move move in moves)
+        {
+            board.MakeMove(move);
+            double score = MiniMax(board, maxDepth);
+            if (isBetter(score, bestScore)) (bestScore, bestMove) = (score, move);
+            board.UndoMove(move);
+        }
+
+        return bestMove;
+    }
+
+    /// <summary>
+    /// Performs the minimax algorithm up to a maximum search depth.
+    /// </summary>
+    /// <param name="board">Current board state.</param>
+    /// <param name="maxDepth">Maximum search depth. Values less than 0 are treated as infinity.</param>
+    /// <returns>Minimax value of board state.</returns>
+    private double MiniMax(Board board, int maxDepth)
+    {
+        if (maxDepth == 0 || board.IsGameOver()) return Heuristic(board);
+        
+        if (maxDepth > 0) maxDepth--;
+
+        SimpleComparison isBetter = board.IsWhiteToMove ? IsGreater : IsLess;
+        double bestScore = board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
+        
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            double score = MiniMax(board, maxDepth);
+            if (isBetter(score, bestScore)) bestScore = score;
+            board.UndoMove(move);
+        }
+
+        return bestScore;
+    }
+
+    /// <summary>
+    /// Heuristic for evaluating board states.
+    /// Based on https://www.chess.com/terms/chess-piece-value.
+    /// </summary>
+    /// <param name="board">Board state.</param>
+    /// <returns>Heuristic score.</returns>
+    private double Heuristic(Board board)
+    {
+        if (board.IsInCheckmate()) return board.IsWhiteToMove ? -100 : 100;
+        if (board.IsDraw()) return 0;
+
+        PieceList[] pieces = board.GetAllPieceLists();
+        double score = (pieces[0].Count - pieces[6].Count) + 
+                       (pieces[1].Count - pieces[7].Count) * 3 + 
+                       (pieces[2].Count - pieces[8].Count) * 3 + 
+                       (pieces[3].Count - pieces[9].Count) * 5 + 
+                       (pieces[4].Count - pieces[10].Count) * 9;
+
+        return score;
+    }
+}
+
+internal static class BoardExtensions
+{
+    public static bool IsGameOver(this Board board) => board.IsInCheckmate() || board.IsDraw();
+}
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,75 +4,10 @@ using System.Linq;
 
 public class MyBot : IChessBot
 {
+    MinimaxBot tempBrain = new();
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return BestMove(board, maxDepth: 16)?.Move ?? moves[0];
-    }
-
-    /// <summary>
-    /// Calculates the (approximately) best move for the current player based on the given board state.
-    /// </summary>
-    /// <param name="board">Board to determine best move for.</param>
-    /// <param name="alpha">Minimum score before pruning search branches.</param>
-    /// <param name="beta">Maximum score before pruning search branches.</param>
-    /// <param name="maxDepth">Maximum depth of min-max search. Negative values are treated as infinity.</param>
-    /// <returns>Best move and its associated score, or null if search was terminated early.</returns>
-    private (Move Move, double Score)? BestMove(Board board, double alpha = double.NegativeInfinity, double beta = double.PositiveInfinity, int maxDepth = -1)
-    {
-        Move? bestMove = null;
-        double bestScore = board.IsWhiteToMove ? alpha : beta;
-        int desiredSign = board.IsWhiteToMove ? 1 : -1;
-
-        Move[] moves = board.GetLegalMoves();
-        double[] moveScores = moves.Select(m => EvaluateMove(board, m) * -desiredSign).ToArray();
-        Array.Sort(moveScores, moves);
-
-        if (maxDepth == 0 || moveScores[0] == 100.0 * desiredSign) return (moves[0], moveScores[0]);
-        if (maxDepth > 0) maxDepth--;
-
-        foreach (Move move in moves)
-        {
-            board.MakeMove(move);
-            if (BestMove(board, alpha, beta, maxDepth) is (Move, double) next) {
-                if (next.Score * desiredSign > bestScore * desiredSign) {
-                    (bestMove, bestScore) = next;
-                    if (desiredSign == 1) alpha = bestScore;
-                    else beta = bestScore;
-                }
-            }
-            board.UndoMove(move);
-
-            if (beta < alpha) return null;
-        }
-
-        return bestMove == null ? null : ((Move)bestMove, bestScore);
-    }
-
-    /// <summary>
-    /// Evaluates the given board state and returns a positive or negative score.
-    /// A positive score favors white and a negative score favors black.
-    /// </summary>
-    /// <param name="board">Board to evaluate.</param>
-    /// <returns>Evaluation score.</returns>
-    private double EvaluateBoard(Board board)
-    {
-        return board.IsInCheckmate() ? (board.IsWhiteToMove ? -100.0 : 100.0) : 0.0;
-    }
-
-
-    /// <summary>
-    /// Evaluates the given move based on the given board state and returns a positive or negative score.
-    /// A positive score favors white and a negative score favors black.
-    /// </summary>
-    /// <param name="board">Board state in which move will be made.</param>
-    /// <param name="move">Move to evaluate.</param>
-    /// <returns>Evaluation score.</returns>
-    private double EvaluateMove(Board board, Move move)
-    {
-        board.MakeMove(move);
-        double score = EvaluateBoard(board);
-        board.UndoMove(move);
-        return score;
+        return tempBrain.Think(board, timer);
     }
 }
